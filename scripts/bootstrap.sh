f9c6d9a56faa219c4dbed8968232e273be09bc3e
zinsani <zinsani@gmail.com>
2016-07-12 23:34:19 +0900

    fix script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,6 @@
 cd ~
 
-[-d backup_dotfiles] || mkdir backup_dotfiles
+[ -d backup_dotfiles ] || mkdir backup_dotfiles
 mv -f ~/.vimrc ~/backup_dotfiles/.vimrc
 mv -f ~/.bashrc ~/backup_dotfiles/.bashrc
 mv -f ~/.tmux.conf ~/backup_dotfiles/.tmux.conf
