3ef0f6c59271f8d60202c435730c6c366f411e0c
zinsani <zinsani@gmail.com>
2016-10-04 11:21:52 +0900

    add gitconfig file

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,9 +2,13 @@ cd ~
 
 [ -d backup_dotfiles ] || mkdir backup_dotfiles
 mv -f ~/.vimrc ~/backup_dotfiles/.vimrc
+mv -f ~/.zshrc ~/backup_dotfiles/.zshrc
 mv -f ~/.bashrc ~/backup_dotfiles/.bashrc
 mv -f ~/.tmux.conf ~/backup_dotfiles/.tmux.conf
+mv -f ~/.gitconfig ~/backup_dotfiles/.gitconfig
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
