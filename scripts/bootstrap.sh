afd4dad4d57bffe36e85c4917d88e41b96fe108f
zinsani <zinsani@gmail.com>
2017-03-30 14:34:05 +0900

    Add gitignore_global

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,9 +6,11 @@ mv -f ~/.zshrc ~/backup_dotfiles/.zshrc
 mv -f ~/.bashrc ~/backup_dotfiles/.bashrc
 mv -f ~/.tmux.conf ~/backup_dotfiles/.tmux.conf
 mv -f ~/.gitconfig ~/backup_dotfiles/.gitconfig
+mv -f ~/.gitignore_global ~/backup_dotfiles/.gitignore_global
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
