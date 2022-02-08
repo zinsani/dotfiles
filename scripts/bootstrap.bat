59b2a58ff951575229986e0424a9214ce27bcd15
jsp <zinsani@gmail.com>
2022-02-09 01:16:08 +0900

    Itroduce bootstrap scripts for windows

---
+++ scripts/bootstrap.bat
@@ -0,0 +1,23 @@
+cd %userprofile%
+
+mkdir backup_dotfiles
+mkdir backup_dotfiles\nvim
+move .vimrc backup_dotfiles\.vimrc
+move .zshrc backup_dotfiles\.zshrc
+move .bashrc backup_dotfiles\.bashrc
+move .tmux.conf backup_dotfiles\.tmux.conf
+move .gitconfig backup_dotfiles\.gitconfig
+move .gitignore_global backup_dotfiles\.gitignore_global
+
+move %userprofile%\AppData\Local\nvim\init.vim backup_dotfiles\nvim\
+move %userprofile%\AppData\Local\nvim\coc-settings.vim backup_dotfiles\nvim\
+
+mklink  %userprofile%\.vimrc %userprofile%\dotfiles\.vimrc 
+mklink  %userprofile%\.zshrc %userprofile%\dotfiles\.zshrc 
+mklink  %userprofile%\.bashrc %userprofile%\dotfiles\.bashrc 
+mklink  %userprofile%\.tmux.conf %userprofile%\dotfiles\.tmux.conf 
+mklink  %userprofile%\.gitconfig %userprofile%\dotfiles\.gitconfig 
+mklink  %userprofile%\.gitignore_global %userprofile%\dotfiles\.gitignore_global 
+mkdir %userprofile%\AppData\Local\nvim
+mklink %userprofile%\AppData\Local\nvim\init.vim %userprofile%\dotfiles\nvim\init.vim
+mklink %userprofile%\AppData\Local\nvim\coc-settings.vim %userprofile%\dotfiles\nvim\coc-settings.vim
