432bf92b1e3923238719727d74548f5781b0b47c
jsp <zinsani@gmail.com>
2023-10-06 17:19:25 +0900

    Enable autocmds for dadbod auto completion on query

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -1,6 +1,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Add any additional autocmds here
+--
+vim.cmd(
+  [[ autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} }) ]]
+)
 
 -- local format_is_enabled = false
 --
