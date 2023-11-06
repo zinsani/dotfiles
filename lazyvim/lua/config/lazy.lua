1324b3c8bfee45ab272cbcee324bf3a69b548676
jsp <zinsani@gmail.com>
2023-11-06 16:56:31 +0900

    Add test.core lazy extension and replace null-ls plugin with lazyvim extension within lazy.lua

--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -16,6 +16,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.coding" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
