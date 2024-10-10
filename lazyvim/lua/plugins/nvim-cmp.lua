5c5deda58267f49b867b70d6021a4b033fd0e4be
jsp <zinsani@gmail.com>
2024-10-10 11:01:16 +0900

    Add keymaps for nvim-cmp

--- lazyvim/lua/plugins/nvim-cmp.lua
+++ lazyvim/lua/plugins/nvim-cmp.lua
@@ -23,4 +23,49 @@ return {
 
     table.insert(opts.sources, { name = "luasnip" })
   end,
+
+  mapping = function(_, mappings)
+    local luasnip = require("luasnip")
+    local cmp = require("cmp")
+
+    table.insert(mappings, {
+
+      -- ... Your other mappings ...
+      ["<CR>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          if luasnip.expandable() then
+            luasnip.expand()
+          else
+            cmp.confirm({
+              select = true,
+            })
+          end
+        else
+          fallback()
+        end
+      end),
+
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.locally_jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      -- ... Your other mappings ...
+    })
+  end,
 }
