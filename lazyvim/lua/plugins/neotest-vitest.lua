76cb31853928203290bcf795cad68c5aefc46569
jsp <zinsani@gmail.com>
2026-01-03 11:35:08 +0900

    vim: neotest-vitest: exclude node_modules

--- lazyvim/lua/plugins/neotest-vitest.lua
+++ lazyvim/lua/plugins/neotest-vitest.lua
@@ -4,8 +4,14 @@ return {
     "marilari88/neotest-vitest",
   },
   opts = {
+    log_level = vim.log.levels.DEBUG,
     adapters = {
-      ["neotest-vitest"] = {},
+      ["neotest-vitest"] = {
+        -- filter_dir to exclude node_modules
+        filter_dir = function(name)
+          return name ~= "node_modules"
+        end,
+      },
     },
   },
 }
