c152f5b5903dc0ab5d4d3e1531610ed220550e39
jsp <zinsani@gmail.com>
2026-01-16 15:37:39 +0900

    fix: vue 2.7 lsp configuration for lazyvim

--- lazyvim/lua/plugins/vue.lua
+++ lazyvim/lua/plugins/vue.lua
@@ -4,68 +4,38 @@ return {
     opts = {
       servers = {
         vuels = { enabled = false },
+        vtsls = { enabled = false },
         ts_ls = { enabled = false },
-        vue_ls = { enabled = false },
+        volar = { enabled = false }, -- Avoid duplication
 
-        -- Configure VTSLS (Preferred Server)
-        vtsls = {
+        -- Configure 'vue_ls' (Volar) for Vue 2 Takeover Mode
+        vue_ls = {
           enabled = true,
-          cmd = {
-             "node",
-             "--max-old-space-size=8192",
-             "/Users/jsp/.local/share/nvim/mason/packages/vtsls/node_modules/@vtsls/language-server/bin/vtsls.js",
-             "--stdio",
-          },
-          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-          settings = {
-            vtsls = {
-              autoUseWorkspaceTsdk = true,
-              tsserver = {
-                globalPlugins = {
-                  {
-                    name = "@vue/typescript-plugin",
-                    location = "/Users/jsp/.nvm/versions/node/v22.11.0/lib/node_modules/@vue/typescript-plugin",
-                    languages = { "vue" },
-                    configNamespace = "typescript",
-                    enableForWorkspaceTypeScriptVersions = true,
-                  },
-                },
-              },
-            },
-          },
-        },
-
-        -- Configure Volar (Hybrid Mode)
-        volar = {
-          enabled = true,
-          cmd = { 
-             "node", 
-             "--max-old-space-size=8192", 
-             "/Users/jsp/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/bin/vue-language-server.js", 
-             "--stdio" 
-          },
-          filetypes = { "vue" },
+          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
           init_options = {
             vue = {
-              hybridMode = true,
+              hybridMode = false,
             },
             typescript = {
-              tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+              tsdk = "/Users/jsp/workspace/soomgo/soomgo-frontend/node_modules/typescript/lib",
             },
           },
-          settings = {
-             vue = {
-                version = 2, 
-             }
-          }
+          -- CRITICAL FIX: Override the default on_init which expects hybrid mode
+          -- This prevents nvim-lspconfig from complaining about missing ts_ls/vtsls
+          on_init = function(client)
+            -- We do nothing here, effectively disabling the default hybrid mode check
+            return true
+          end,
         },
       },
-      
       setup = {
-        vtsls = function(_, opts) return nil end, -- Enable vtsls
-        ts_ls = function() return true end, -- Disable ts_ls
-        volar = function(_, opts) return nil end, -- Enable volar
-        vue_ls = function() return true end, -- Disable deprecated
+        -- The Kill Switch: Prevent vtsls and ts_ls from starting
+        vtsls = function()
+          return true
+        end,
+        ts_ls = function()
+          return true
+        end,
       },
     },
   },
